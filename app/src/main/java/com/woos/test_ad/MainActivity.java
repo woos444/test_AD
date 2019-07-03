cfc7b025143e7ee1964f10dfa6f03ab906e2e3a4
Wolf <Wolf@DESKTOP-21SVAEG>
2019-07-03 17:37:32 +0900

    버튼으로 마커 on/off

--- app/src/main/java/com/woos/test_ad/MainActivity.java
+++ app/src/main/java/com/woos/test_ad/MainActivity.java
@@ -1,9 +1,12 @@
 package com.woos.test_ad;
 import androidx.annotation.NonNull;
 import androidx.annotation.UiThread;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.naver.maps.geometry.LatLng;
 import com.naver.maps.geometry.LatLngBounds;
@@ -20,7 +23,9 @@ import com.naver.maps.map.overlay.PathOverlay;
 import java.sql.Array;
 import java.util.Arrays;
 
+
 public class MainActivity extends AppCompatActivity implements  OnMapReadyCallback{
+    boolean a=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +38,54 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
             mapFragment = MapFragment.newInstance();
             fm.beginTransaction().add(R.id.map, mapFragment).commit();
         }
-
         mapFragment.getMapAsync(this);
     }
 
 
+    public void onMapReady(@NonNull final NaverMap naverMap) {
 
-    public void onMapReady(@NonNull NaverMap naverMap) {
 
 
         naverMap.setMapType(NaverMap.MapType.Hybrid);//맵타입 변경
+
+        //마커생성
+        final Marker ksU_M = new Marker();
+        final Marker ksS_M = new Marker();
+        final Marker jbnu_M = new Marker();
+        final Marker wku_M = new Marker();
+
+        final InfoWindow kunU = new InfoWindow();
+        kunU.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "군산대";
+            }
+        });
+        final InfoWindow kunS = new InfoWindow();
+        kunS.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "군산시청";
+            }
+        });
+        final InfoWindow jbU = new InfoWindow();
+        jbU.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "전북대";
+            }
+        });
+        final InfoWindow wkU = new InfoWindow();
+        wkU.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
+            @NonNull
+            @Override
+            public CharSequence getText(@NonNull InfoWindow infoWindow) {
+                return "원광대";
+            }
+        });
 
         //좌표설정
 
@@ -55,49 +98,8 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
         LatLngBounds bounds2 =new LatLngBounds(wku,kunsanS);
         LatLngBounds bounds = new LatLngBounds(bounds1.getCenter(),bounds2.getCenter());
 
-
         CameraPosition cameraPosition = new CameraPosition(bounds.getCenter(),9);//카메라중앙설정 및 줌값 설정
         naverMap.setCameraPosition(cameraPosition);//네이버 맵에 카메라설정값 적용
-
-        InfoWindow kunU = new InfoWindow();
-        kunU.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "군산대";
-            }
-        });
-        InfoWindow kunS = new InfoWindow();
-        kunS.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "군산시청";
-            }
-        });
-        InfoWindow jbU = new InfoWindow();
-        jbU.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "전북대";
-            }
-        });
-        InfoWindow wkU = new InfoWindow();
-        wkU.setAdapter(new InfoWindow.DefaultTextAdapter(this) {
-            @NonNull
-            @Override
-            public CharSequence getText(@NonNull InfoWindow infoWindow) {
-                return "원광대";
-            }
-        });
-
-
-        //마커생성
-        Marker ksU_M = new Marker();
-        Marker ksS_M = new Marker();
-        Marker jbnu_M = new Marker();
-        Marker wku_M= new Marker();
 
         //마커위치 등록
         ksU_M.setPosition(kunsanU);
@@ -105,22 +107,49 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
         jbnu_M.setPosition(jbnu);
         wku_M.setPosition(wku);
 
-        //네이버 맵에 마커 표시
-        ksU_M.setMap(naverMap);
-        ksS_M.setMap(naverMap);
-        jbnu_M.setMap(naverMap);
-        wku_M.setMap(naverMap);
-
-        kunU.open(ksU_M);
-        kunS.open(ksS_M);
-        jbU.open(jbnu_M);
-        wkU.open(wku_M);
-
-
-        PathOverlay path = new PathOverlay();
-        path.setCoords(Arrays.asList(kunsanU,kunsanS,wku,jbnu));
+        //마커끼리 선연결
+        final PathOverlay path = new PathOverlay();
+        path.setCoords(Arrays.asList(kunsanU, kunsanS, wku, jbnu));
         path.setWidth(30);
-        path.setMap(naverMap);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(a==true)
+                {
+                    //네이버 맵에 마커 표시
+                    ksU_M.setMap(naverMap);
+                    ksS_M.setMap(naverMap);
+                    jbnu_M.setMap(naverMap);
+                    wku_M.setMap(naverMap);
+                    path.setMap(naverMap);
+
+                    //마커에 설명문 추가
+                    kunU.open(ksU_M);
+                    kunS.open(ksS_M);
+                    jbU.open(jbnu_M);
+                    wkU.open(wku_M);
+
+                    button.setText("on");
+                    a=false;
+
+                }
+                else if(a==false) {
+                    ksU_M.setMap(null);
+                    ksS_M.setMap(null);
+                    jbnu_M.setMap(null);
+                    wku_M.setMap(null);
+
+                    path.setMap(null);
+
+                    button.setText("off");
+                    a=true;
+                }
+
+            }
+        });
 
     }
 }
